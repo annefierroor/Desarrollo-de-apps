49c2725b8b51349521bd12a7c8f27412ce234bb9
annefierroor <anne_fierroor@fet.edu.co>
2025-08-27 20:53:45 -0500

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,9 +1,24 @@
 function agregar() {
     let texto = document.getElementById("inputTexto").value;
+
     if (texto) {
-        let elemento = document.createElement("p");
-        elemento.textContent = texto;
+    
+        let elemento = document.createElement("div");
+
+        let parrafo = document.createElement("p");
+        parrafo.textContent = texto;
+
+        let botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.onclick = function () {
+            elemento.remove(); 
+        };
+
+        elemento.appendChild(parrafo);
+        elemento.appendChild(botonEliminar);
+
         document.getElementById("contenedor").appendChild(elemento);
-        document.getElementById("inputTexto").value = "";
-    }
+
+        document.getElementById("inputTexto").value = "";
+    }
 }
